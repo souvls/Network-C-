0563d5b8b88eefe651197ac500c356d7ea78e9fd
Sou Vls <soulixai.vls@gmail.com>
2023-02-16 14:57:04 +0700

    first commit

---
+++ BTH2/Cau1_Client_SentNumberToSever/Cau1_Client_SentNumberToSever/Sever.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Cau1_Client_SentNumberToSever
+{
+    internal class Program
+    {
+        private const int PORT_NUMBER = 9999;
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8; 
+            try
+            {
+                IPAddress address = IPAddress.Parse("127.0.0.1"); //ລະບຸຊື່ເຄຶ່ອງຜູ້ໃຊ້ປາຍທາງ
+                TcpListener listener = new TcpListener(address, PORT_NUMBER); //ສື່ສານ ຊ່ອງທີ 9999
+
+                // 1. listen ຖ້າຜູ້ໃຊ້ເຂົ້າເຊື່ອມຕໍ່
+                listener.Start();
+                Console.WriteLine("*** Server started on " + listener.LocalEndpoint);
+                Console.WriteLine("*** Đang đợi client kết nối.");
+
+                 //ຜູ້ໃຊ້ເຊື່ອມຕໍ່ແລ້ວ
+                Socket socket = listener.AcceptSocket();
+                Console.WriteLine("*** Clien kết nối:" + socket.RemoteEndPoint);
+
+                 //ກຽມຮັບສົ່ງຂໍ້ມູນ
+                var stream = new NetworkStream(socket);
+                var reader = new StreamReader(stream);
+                var writer = new StreamWriter(stream);
+                writer.AutoFlush = true;
+
+                while (true)
+                {
+                    string[] arrayStr;
+                    // 2. receive ຮັບຂໍ້ມູນ
+                    string str = reader.ReadLine();
+                    if (str.ToUpper() == "EXIT") //ຖ້າຜູ້ໃຊ້ສົ່ງ EXIT ມາ, ໃຫ້ຢຸດໂປແກຣມທັນທີ
+                    {
+                        // 3. send ສົ່ງຂໍ້ມູນ
+                        writer.WriteLine("bye");
+                        break;
+                    }
+                    else //ຖ້າບໍ່ແມ່ນ EXIT
+                    {
+                        arrayStr = str.Split(','); // ແຍກຂໍ້ຄວາມດ້ວຍ , ແລ້ວເກັບໄວ້ໃນ arrayStr
+
+                        //ແປງຂໍ້ຄວາມເປັນໂຕເລກແລ້ວໄລ່
+                        double a = double.Parse(arrayStr[0]);
+                        double b = double.Parse(arrayStr[1]);
+                        double result = 0;
+                        
+
+                        switch (arrayStr[2]) {
+                            case "+": result = a + b; break;
+                            case "-": result = a - b; break;
+                            case "*": result = a * b; break;
+                            case "/": result = a / b; break;
+                            default: result = 0; break;
+                        }
+                        // 3. send ສົ່ງຂໍ້ມູນ
+                        writer.WriteLine("Server: " + a+" " + arrayStr[2]+" "+b+" = "+result);
+
+                    }
+                    
+                }
+                // 4. close ປິດການເຊື່ອມຕໍ່
+                stream.Close();
+                socket.Close();
+                listener.Stop();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex);
+            }
+            Console.Read();
+        }
+    }
+}
---
+++ BTH2/Cau1_Client_SentNumberToSever/ClientApp/Client.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientApp
+{
+    internal class Client
+    {
+        private const int PORT_NUMBER = 9999;
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            try
+            {
+                
+                TcpClient client = new TcpClient();//ປະກາດຜູ້ໃຊ້
+
+                // 1. connect ເຊື່ອມຕໍ່ໄປ ຊ່ອງສື່ສານ 9999
+                client.Connect("127.0.0.1", PORT_NUMBER);
+                Stream stream = client.GetStream();
+                Console.WriteLine("*** Kết nối sever thành công.");//ເຊື່ອມຕໍ່ server ສຳເລັດ
+
+                
+                var reader = new StreamReader(stream);//ກຽມຮັບສົ່ງຂໍ້ມູນ 27-29
+                var writer = new StreamWriter(stream);
+                writer.AutoFlush = true;
+
+                while (true)
+                {    
+                    Console.Write("Nhập số nguyên a (gõ \"EXIT\" để thoát): "); //ຮັບຂໍ້ຄວມຈາກຄີບອດ
+                    string strNum1 = Console.ReadLine().Trim();
+                    string massage = strNum1;
+                    if (strNum1 != "EXIT")//ກວດສອບ ຂໍ້ຄວມຮັບຈາກຄີບອດ ແມ່ນ Exit ຫຼືບໍ່
+                    {
+                        Console.Write("Nhập số nguyên b: ");
+                        string strNum2 = Console.ReadLine().Trim();
+
+                        Console.Write("Nhập phép toán [+,-,*,/]: ");  //ຮັບບວກລົບຄູນຫານ ຈາກຄີບອດ
+                        char math = Convert.ToChar(Console.ReadLine());
+                        massage = strNum1 + "," + strNum2 + "," + math; //ລວມຂໍ້ມູນເປັນ String ອັນດຽວ 
+                    }
+
+                    //2.Sent ສົ່ງຂໍ້ຄວາມໄປ server
+                    writer.WriteLine(massage);
+
+                    // 3. receive ຮັບຂໍ້ຄວາມຈາກ sever
+                    string str = reader.ReadLine();
+
+                    if (str.ToUpper() == "BYE") //ຖ້າ sver ບອກ BYE ໃຫ້ຢຸດໂປແກຣມ
+                        break;
+                    Console.WriteLine(str); //ສະແດງຂໍ້ມູນເທິງຈໍ
+                    Console.WriteLine("_____________________________________________\n");
+                }
+                // 4. close ປິດການເຊື່ອມຕໍ່
+                stream.Close();
+                client.Close();
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex);
+            }
+
+            Console.Read();
+        }
+    }
+}
